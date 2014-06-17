e1e48a0ea0032f704ad85af437ea00d22023840f
Zetozz <olsz_wroc_dom@poczta.fm>
2014-06-17 11:09:59 +0200

    Message jakis

---
+++ Dzien37/DokumentyPacjenci/Class1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DokumentyPacjenci
+{
+    public class Class1
+    {
+        void a()
+        {
+            static int i = 0;
+            string Imie;
+
+        }
+
+    }
+}
